164783ce8f8046b755089fd88d9b0b855eb419b6
Adrian <adriusz@gmail.com>
2019-03-30 13:04:19 +0100

    Workshopmembers commit2

--- src/main/java/szok_hb/tables/Workshopmembers.java
+++ src/main/java/szok_hb/tables/Workshopmembers.java
@@ -1,4 +1,29 @@
 package szok_hb.tables;
 
+import lombok.Data;
+import org.hibernate.criterion.Order;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table (name = "workshopmembers")
 public class Workshopmembers {
+
+    @Id
+    private int id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "participantsID")
+    private Participant participantsID;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "workshopID")
+    private Workshops workshopID;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "orderID")
+    private Order orderID;
+
+    private boolean payed;
 }
